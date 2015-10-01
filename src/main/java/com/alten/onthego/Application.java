ef1e2ea61769f18f7f560655ee0cefdee52ada6b
khaled87 <khaled.nawasreh@gmail.com>
2015-10-01 14:02:45 +0200

    skeleton for maven and spring_boot

---
+++ src/main/java/com/alten/onthego/Application.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.alten.onthego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author ka3146
+ */
+@SpringBootApplication
+public class Application {
+        public static void main( String[] args ) throws Exception
+    {
+       SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/alten/onthego/controllers/Greeting.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.alten.onthego.controllers;
+
+import java.math.BigInteger;
+
+/**
+ *
+ * @author ka3146
+ *//*
+
+import java.math.BigInteger;
+
+/**
+ *
+ * @author Khaled
+ */
+public class Greeting {
+
+    private BigInteger id;
+    private String text;
+
+    public Greeting() {
+
+    }
+
+    public void setId(BigInteger id) {
+        this.id = id;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public BigInteger getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+}
