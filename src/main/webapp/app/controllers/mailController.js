e07d797bed3575980717274f51fcc4a30fe21a84
Vasileios Golematis <v.golematis@gmail.com>
2015-10-04 16:52:14 +0200

    added controller

---
+++ src/main/webapp/app/controllers/mailController.js
@@ -0,0 +1,12 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* global app */
+
+app.controller("mailCtrl", function($scope){
+    $scope.email = "@alten.se";
+});
+
--- src/main/webapp/app/onTheGo.js
+++ src/main/webapp/app/onTheGo.js
@@ -3,5 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+/* global angular */
+
 var app = angular.module('onTheGoApp', []);
 
