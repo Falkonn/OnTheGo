84dca26b1bf335181940dbadf885d6bd20bedb00
Mattias Isene <mattias.isene@alten.se>
2015-10-09 10:53:00 +0200

    Front end - first pages layout

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -1,5 +1,4 @@
 'use strict';
-
 /* 
  *  The Shop App
  */
@@ -9,13 +8,46 @@ var OnTheGo = angular.module('OnTheGo', [
 
 OnTheGo.config(['$routeProvider',
     function ($routeProvider) {  // Injected object $routeProvider
+        //console.log("In the routeProvider");
         $routeProvider.
-                when('/index', {
-                    templateUrl: 'index.html',
-                    controller: 'mailController'
-                }).
-                otherwise({
-                    redirectTo: 'index.html'
-                });
+            when('/', {
+                templateUrl: 'partials/welcome.html',
+                controller: 'mailController'
+            }).
+            when('/register', {
+                templateUrl: 'partials/register.html',
+                controller: 'mailController'
+            }).
+            when('/confirmation', {
+                templateUrl: 'confirmation.html',
+                controller: 'confirmController'
+            }).
+            when('/main', {
+                templateUrl: 'main.html',
+                controller: 'mainController'
+            }).
+            when('/main/team', {
+                templateUrl: 'team.html',
+                controller: 'mainController'
+            }).
+            when('/main/assignments', {
+                templateUrl: 'assignments.html',
+                controller: 'mainController'
+            }).
+            when('/main/scoreboard', {
+                templateUrl: 'scoreboard.html',
+                controller: 'mainController'
+            }).
+            when('/main/schedule', {
+                templateUrl: 'schedule.html',
+                controller: 'mainController'
+            }).
+            when('/main/map', {
+                templateUrl: 'map.html',
+                controller: 'mainController'
+            }).
+            otherwise({
+                redirectTo: 'index.html'
+            });       
     }]);
 
