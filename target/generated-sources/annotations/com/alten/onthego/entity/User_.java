e92f763665e29b726c1923c599934a5b80344c2e
khaled87 <khaled.nawasreh@gmail.com>
2015-10-12 17:48:30 +0200

    fix bug

--- target/generated-sources/annotations/com/alten/onthego/entity/User_.java
+++ target/generated-sources/annotations/com/alten/onthego/entity/User_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2015-10-12T17:30:41")
+@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2015-10-12T17:41:54")
 @StaticMetamodel(User.class)
 public class User_ { 
 
