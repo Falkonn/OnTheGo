a2bd38113ab803e397672a07713631a10549fa0a
Mattias Isene <mattias.isene@alten.se>
2015-10-13 10:53:11 +0200

    Frontend rebase

--- src/main/webapp/mainController.js
+++ src/main/webapp/mainController.js
@@ -1,22 +1,21 @@
 
-var mainModule = angular.module('mainModule', [])
-
+var mainModule = angular.module('mainModule', ['ui.bootstrap'])
 
 .controller('mainController',['$scope','$http', function ($scope, $http) {
     var mc = $scope;
     mc.loggedIn = true;
     
-    $scope.assignmentConfirmation = "Glöm inte att du måste kunna bevisa att \n\
+    mc.assignmentConfirmation = "Glöm inte att du måste kunna bevisa att \n\
         du/gruppen har utfört uppdraget.";
     
     /////////////////////// UPPGIFTER
-    $scope.assignments = {
+    mc.assignments = {
         "numberOfAssignments": 10,
         "tasks": [
             {
                 "id": 1,
-                "name": "Bestäm ett forum att kommunicera via",
-                "details": "För att gruppen ska kunna kommunicera och lära känna varandra, behöver ni hitta ett gemensamt forum för kommunikation.",
+                "name": "Forum att kommunicera via",
+                "description": "För att gruppen ska kunna kommunicera och lära känna varandra, behöver ni hitta ett gemensamt forum för kommunikation.",
                 "type": "grupp",
                 "estimation": "2 minuter",
                 "points": 10,
@@ -25,7 +24,7 @@ var mainModule = angular.module('mainModule', [])
             {
                 "id": 2,
                 "name": "Gilla Alten Sweden på LinkedIn",
-                "details": "Logga in på LinkedIn, sök på Alten Sweden och gilla. Om du redan har gillat Alten Sweden kan du också bocka för uppgiften.",
+                "description": "Logga in på LinkedIn, sök på Alten Sweden och gilla. Om du redan har gillat Alten Sweden kan du också bocka för uppgiften.",
                 "type": "personlig",
                 "estimation": "1 minut",
                 "points": 10,
@@ -34,7 +33,7 @@ var mainModule = angular.module('mainModule', [])
             {
                 "id": 3,
                 "name": "Skicka en selfie",
-                "details": "Ta en selfie och ladda upp. När du ser att bilden finns i gruppvyn kan du bocka för uppgiften.",
+                "description": "Ta en selfie och ladda upp. När du ser att bilden finns i gruppvyn kan du bocka för uppgiften.",
                 "type": "personlig",
                 "estimation": "1-5 minuter",
                 "points": 10,
@@ -43,7 +42,7 @@ var mainModule = angular.module('mainModule', [])
             {
                 "id": 4,
                 "name": "Bli vänner på LindedIn",
-                "details": "I gruppvyn kan du se vilka personer som är medlemmar i din grupp (om du vill göra detta innan ni har upprättat kontakt). Sök upp de på LinkedIn och bli vänner med de. När du blivit vänner med alla som har LinkedIn i din grupp kan du bocka för uppgiften.",
+                "description": "I gruppvyn kan du se vilka personer som är medlemmar i din grupp (om du vill göra detta innan ni har upprättat kontakt). Sök upp de på LinkedIn och bli vänner med de. När du blivit vänner med alla som har LinkedIn i din grupp kan du bocka för uppgiften.",
                 "type": "personlig",
                 "estimation": "1-10 minuter",
                 "points": 10,
@@ -52,7 +51,7 @@ var mainModule = angular.module('mainModule', [])
             {
                 "id": 5,
                 "name": "Designa en Alten-drink",
-                "details": "Vad tycker du vore en god och passande drink för Alten?",
+                "description": "Vad tycker du vore en god och passande drink för Alten?",
                 "type": "personlig",
                 "estimation": "1-10 minuter",
                 "points": 10,
