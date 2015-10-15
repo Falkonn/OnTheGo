8f1436910d4cac84093d1376a86718a657e0ef7c
khaled87 <khaled.nawasreh@gmail.com>
2015-10-15 14:38:42 +0200

    updated models

--- src/main/java/com/alten/onthego/model/ScoreInfo.java
+++ src/main/java/com/alten/onthego/model/ScoreInfo.java
@@ -7,6 +7,7 @@ package com.alten.onthego.model;
 
 import com.alten.onthego.entity.Score;
 import java.util.Collection;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -26,14 +27,19 @@ public class ScoreInfo {
         return (Collection<Score>) scoresquery.getResultList();
     }
 
-    public Collection<Score> getScoresbyTeamId(int teamId) {
-        Query scoresquery = em.createQuery("SELECT s FROM Score s where s.teamId =" + "\"" + teamId + "\"");
-        return (Collection<Score>) scoresquery.getResultList();
+    public List<Score> getScoresbyTeamId(int teamId) {
+        Query scoresquery = em.createQuery("SELECT s FROM Score s where s.teamId =" + teamId);
+        return (List<Score>) scoresquery.getResultList();
     }
 
     public Collection<Score> getScoresbyUserId(int userId) {
-        Query scoresquery = em.createQuery("SELECT s FROM Score s where s.userId =" + "\"" + userId + "\"");
+        Query scoresquery = em.createQuery("SELECT s FROM Score s where s.userId =" + userId );
         return (Collection<Score>) scoresquery.getResultList();
+    }
+    
+       public List<Score> getScoresbyTaskId(int taskId) {
+        Query scoresquery = em.createQuery("SELECT s FROM Score s where s.task =" + taskId);
+        return (List<Score>) scoresquery.getResultList();
     }
 
 }
