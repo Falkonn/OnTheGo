4dde94e35d5caf4a71f11bda46b52bbf55041370
Vasileios Golematis <v.golematis@gmail.com>
2015-10-16 08:48:10 +0200

    Navigation headings

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -26,31 +26,37 @@ OnTheGo.config(['$routeProvider', '$locationProvider',
                 controllerAs: 'regCtrl'
             }).
             when('/info', {
+                title: 'App Info',
                 templateUrl: 'partials/appinfo.html',
                 controller: 'mainController',
                 controllerAs: 'mainCtrl'
             }).
             when('/team', {
+                title: 'MITT TEAM',
                 templateUrl: 'partials/team.html',
                 controller: 'mainController',
                 controllerAs: 'mainCtrl'
             }).
             when('/assignments', {
+                title: 'Uppgifter',
                 templateUrl: 'partials/assignments.html',
                 controller: 'mainController',
                 controllerAs: 'mainCtrl'
             }).
             when('/scoreboard', {
+                title: 'Pöangtavla',
                 templateUrl: 'partials/scoreboard.html',
                 controller: 'mainController',
                 controllerAs: 'mainCtrl'
             }).
             when('/schedule', {
+                title: 'Konferensinfo',
                 templateUrl: 'partials/schedule.html',
                 controller: 'mainController',
                 controllerAs: 'mainCtrl'
             }).
             when('/map', {
+                title: 'Festinfo',
                 templateUrl: 'partials/map.html',
                 controller: 'mainController',
                 controllerAs: 'mainCtrl'
@@ -67,3 +73,8 @@ OnTheGo.config(['$routeProvider', '$locationProvider',
         //$locationProvider.html5Mode(true);
     }]);
 
+OnTheGo.run(['$rootScope', function($rootScope) {
+    $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+        $rootScope.title = current.$$route.title;
+        });
+    }]);
